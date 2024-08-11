04ff29798d478c0cc6ccdecbca9d93164a44323a
Kam <erkamyaman35@gmail.com>
2024-08-11 22:01:45 +0300

    lombok.Data-added

--- src/main/java/models/Review.java
+++ src/main/java/models/Review.java
@@ -1,4 +1,14 @@
 package models;
 
+import lombok.Data;
+
+import java.util.Date;
+@Data
 public class Review {
+    private String id;
+    private String title;
+    private String content;
+    private Date createdAt;
+
+
 }
--- src/main/java/models/Xmen.java
+++ src/main/java/models/Xmen.java
@@ -1,47 +1,13 @@
 package models;
 
+import lombok.Data;
+
+@Data
 public class Xmen {
     private String id;
     private String name;
     private String currentLocation;
     private int health;
 
-    public Xmen(String id, String name, String currentLocation, int health) {
-        this.id = id;
-        this.name = name;
-        this.currentLocation = currentLocation;
-        this.health = health;
-    }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public int getHealth() {
-        return health;
-    }
-
-    public void setHealth(int health) {
-        this.health = health;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCurrentLocation() {
-        return currentLocation;
-    }
-
-    public void setCurrentLocation(String currentLocation) {
-        this.currentLocation = currentLocation;
-    }
 }
