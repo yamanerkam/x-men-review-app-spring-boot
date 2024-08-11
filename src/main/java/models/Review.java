9df2fc0da2b0bbd59d7f56f9d7ac3411f77e5a39
Kam <erkamyaman35@gmail.com>
2024-08-11 23:17:50 +0300

    postgresql-added

--- src/main/java/models/Review.java
+++ src/main/java/models/Review.java
@@ -1,10 +1,21 @@
 package models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
     private String title;
     private String content;
--- src/main/java/models/Xmen.java
+++ src/main/java/models/Xmen.java
@@ -1,13 +1,24 @@
 package models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Xmen {
-    private String id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String name;
     private String currentLocation;
     private int health;
 
 
+   
 }
