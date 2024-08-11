b8db26d94a91f36b5b2b3fcd47b2372b7a31c625
Kam <erkamyaman35@gmail.com>
2024-08-11 16:17:07 +0300

    Initial commit

---
+++ src/main/java/com/xmen/xmenreviewapp/XMenReviewAppApplication.java
@@ -0,0 +1,13 @@
+package com.xmen.xmenreviewapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XMenReviewAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XMenReviewAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xmen/xmenreviewapp/XMenReviewAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmen.xmenreviewapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XMenReviewAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
